b5bd986f17cedf60a5953df109ac07bbc5c6979a
Martine23FT <martinemidttveit@hotmail.com>
2024-05-14 15:20:00 +0200

    Wave / Validator
    Changes and adjustments made based on errors in Wave and Validator.

--- src/js/pages/editListing/index.js
+++ src/js/pages/editListing/index.js
@@ -1,8 +1,10 @@
 import listingMedia from "../../pages/newListing/listingMedia.js";
 import descriptionCount from "../../pages/newListing/descriptionCount.js"
-import storage from "../../helpers/storage.js";
 import getListings from "../../api/auth/requests/getListings.js";
 import editListingForm from "../../api/listeners/editListingForm.js";
+import init from "../../updates/init.js";
+
+init();
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
